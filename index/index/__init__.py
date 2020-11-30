7d8435261d48d6a3eccbe4381facfcc82367bd0f
Unser Hashsham <unserh@umich.edu>
2020-11-29 22:35:55 -0500

    Add file reading and basic hits endpoint structure

--- index/index/__init__.py
+++ index/index/__init__.py
@@ -1,8 +1,32 @@
 """Insta485 package initializer."""
-import flask
+from pathlib import Path
 
-# app is a single object used by all the code modules in this package
-app = flask.Flask(__name__)  # pylint: disable=invalid-name
+# Load search files
+inverted_index_file = Path("./inverted_index.txt").open(mode="r")
+pagerank_file = Path("./pagerank.out").open(mode="r")
+stopwords_file = Path("./stopwords.txt").open(mode="r")
+
+# Create usable data structures from files
+inverted_index_idfs = {}
+inverted_index_docs = {}
+for line in inverted_index_file:
+    vals = line.split()
+    inverted_index_idfs[vals[0]] = vals[1]
+    inverted_index_docs[vals[0]] = {}
+    for i in range(start=2, stop=len(vals) step=3):
+        inverted_index_docs[vals[0]][vals[i]] = {
+            "occurrences": vals[i+1],
+            "norm_factor": vals[i+2],
+        }
+
+pagerank = {}
+for line in pagerank_file:
+    pair = line.split(",")
+    pagerank[pair[0]] = pair[1] 
+
+stopwords = []
+for line in stopwords_file:
+    stopwords.append(line)
 
 # Tell our app about views and model.  This is dangerously close to a
 # circular import, which is naughty, but Flask was designed that way.
---
+++ index/index/api/hits.py
@@ -0,0 +1,20 @@
+"""REST API for listing all available services."""
+import flask
+import index
+
+
+@index.app.route('/api/v1/hits/', methods=['GET'])
+def get_services():
+    """Return a list of hits for the user query."""
+    w = flask.request.args.get("w", default=0.5, type=float)
+    query = flask.request.args.get("q", default="", type=str)
+
+    context = {
+      "hits": [
+          {
+              "docid": 0,
+              "score": 0.0
+          }
+      ]
+    }
+    return flask.jsonify(**context)
