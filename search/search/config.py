9e650f0b8510d48427c4990812d83bf529b782c8
Unser Hashsham <unserh@umich.edu>
2020-12-03 17:47:56 -0500

    Complete search UI server app configuration

--- search/search/config.py
+++ search/search/config.py
@@ -1,2 +1,5 @@
 # URL of the index server
 INDEX_API_URL = "http://localhost:8001/api/v1/hits/"
+
+SEARCH_ROOT = pathlib.Path(__file__).resolve().parent
+DATABASE_FILENAME = SEARCH_ROOT/'var'/'wikipedia.sqlite3'
